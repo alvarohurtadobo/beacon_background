3af19e3703d51482b1aec949bc3387bed60127bc
Álvaro Rubén Hurtado Maldonado <a.hurtado.bo@gmail.com>
2023-05-04 17:39:26 -0400

    Saved

--- app/src/main/java/com/bcontrol/app/bcontrol/LoginFragment.kt
+++ app/src/main/java/com/bcontrol/app/bcontrol/LoginFragment.kt
@@ -48,11 +48,7 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         if (myUsername != "" && myPassword != "") {
             Log.d("DEBUG", "Registered data is $myUsername, $myPassword")
             var answer: MyHttpResponse = postJson(
-<<<<<<< HEAD
-                "https://6be3-2800-cd0-ad02-e00-48ad-e0af-864d-1d0c.ngrok-free.app/api/v1/token/",
-=======
                 "$myUrl/api/v1/token/",
->>>>>>> feature/removedDrawer
                 """{"username": "$myUsername", "password": "$myPassword"}"""
             )
             Log.d("DEBUG", "Answer is ${answer.statusCode}:, ${answer.response}")
@@ -60,16 +56,7 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
                 myUser = UserModel.fromJsonString(answer.response)
                 Log.d("DEBUG","Direct user print $myUser")
                 Log.d("DEBUG","To String user print ${myUser.toString()}")
-<<<<<<< HEAD
-
-                if(myUser.roleId==1) {
-                    findNavController().navigate(R.id.action_loginFragment_to_homeSupervisorFragment)
-                }else{
-                    findNavController().navigate(R.id.action_loginFragment_to_homeWorkerFragment)
-                }
-=======
                 findNavController().navigate(R.id.action_loginFragment_to_monitoringFragment)
->>>>>>> feature/removedDrawer
             } else {
                 val editor = sharedPreferences?.edit()
                 editor?.putString("username", "")
--- app/src/main/java/com/bcontrol/app/bcontrol/ProfileFragment.kt
+++ app/src/main/java/com/bcontrol/app/bcontrol/ProfileFragment.kt
@@ -6,30 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
-<<<<<<< HEAD
-
-class ProfileFragment : Fragment(R.layout.fragment_profile) {
-    lateinit var firstName: EditText
-    lateinit var lastName: EditText
-    lateinit var email: EditText
-    lateinit var role: EditText
-    lateinit var client: EditText
-    lateinit var branch: EditText
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        firstName =requireView().findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.firstname_edit_text)
-        lastName =requireView().findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.lastname_edit_text)
-        email =requireView().findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.email_edit_text)
-        role =requireView().findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.role_edit_text)
-        client =requireView().findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.company_edit_text)
-        branch =requireView().findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.branch_edit_text)
-        firstName.setText(myUser.firstName)
-        lastName.setText(myUser.lastName)
-        email.setText(myUser.email)
-        role.setText(myUser.roleName)
-        client.setText(myUser.clientName)
-        branch.setText(myUser.branchName)
-=======
 import android.widget.TextView
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
@@ -65,6 +41,5 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
         client_input.setText(myUser.clientName)
         branch_input = requireView().findViewById<EditText>(R.id.branch_edit_text)
         branch_input.setText(myUser.branchName)
->>>>>>> feature/removedDrawer
     }
 }
